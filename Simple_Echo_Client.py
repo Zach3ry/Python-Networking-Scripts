53dd8e0552f11a56dbe9e52619397d4645ee139c
Zachery D <zachery.degraffenreid@outlook.com>
2018-10-18 09:20:12 -0400

    Update Simple_Echo_Client.py

--- Simple_Echo_Client.py
+++ Simple_Echo_Client.py
@@ -2,8 +2,8 @@
 
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+HOST = '127.0.0.1'  # The server's IP address...IP works better than the host name
+PORT = 65432        # Port the server is listening on
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
